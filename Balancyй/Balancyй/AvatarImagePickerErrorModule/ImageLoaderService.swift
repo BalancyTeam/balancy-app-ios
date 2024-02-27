85ff45563926f2af260da8df59d048b885b668ad
ToBiSiD <gerolbt19855@gmail.com>
2024-02-27 14:13:05 +0200

    rename method on loadService Protocol

--- Balancyй/Balancyй/AvatarImagePickerErrorModule/ImageLoaderService.swift
+++ Balancyй/Balancyй/AvatarImagePickerErrorModule/ImageLoaderService.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 protocol LoaderService {
-    func loadData() async throws
+    func loadImage() async throws
 }
 
 final class ImageLoaderService: LoaderService {
@@ -9,11 +9,12 @@ final class ImageLoaderService: LoaderService {
     private let testLink: String = ""
     private(set) var loadedURL: String = ""
     
-    func loadData() async throws  {
+    func loadImage() async throws  {
         guard let imageUrl = URL(string: testLink) else {
             throw NSError(domain: "Invalid URL", code: 0, userInfo: nil)
         }
         
+        var fileURL: URL?
         do {
             let (data, _) = try await URLSession.shared.data(from: imageUrl)
             
@@ -21,12 +22,15 @@ final class ImageLoaderService: LoaderService {
                                                                  in: .userDomainMask,
                                                                  appropriateFor: nil,
                                                                  create: true)
-            let fileURL = documentsDirectory.appendingPathComponent(finalImageName)
-            try data.write(to: fileURL)
-            self.loadedURL = fileURL.absoluteString
+            fileURL = documentsDirectory.appendingPathComponent(finalImageName)
+            try data.write(to: fileURL!)
         } catch {
             self.loadedURL = ""
             throw error
+        }
+        
+        if let absoluteURL = fileURL?.absoluteString {
+            self.loadedURL = absoluteURL
         }
     }
 }
