47cc07ff8d61d3a2331c867e83c5ff08c97a1145
ToBiSiD <gerolbt19855@gmail.com>
2024-07-06 14:45:12 +0300

    add mainScreenViewContoller and folder to MainScreen feature

---
+++ Balancyй/Balancyй/MainScreen/MainScreenViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MainScreenViewController.swift
+//  Balancy
+//
+//  Created by Tobias on 06.07.2024.
+//
+
+import UIKit
+
+final class MainScreenViewController: UIViewController {
+    
+}
