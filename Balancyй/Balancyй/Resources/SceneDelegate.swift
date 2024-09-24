fd8d9fbc9937317ac97d8493198d5f6e1b94a73b
Oleksandr Toropov <toropov.oleksandr03@gmail.com>
2024-09-24 23:56:36 +0300

    fixed sceneDelegate

--- Balancyй/Balancyй/Resources/SceneDelegate.swift
+++ Balancyй/Balancyй/Resources/SceneDelegate.swift
@@ -16,21 +16,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-
-        let window = UIWindow(windowScene: windowScene)
-        
-        // Initialize the UserHoursViewController
-        let userHoursVC = UserHoursViewController()
-        
-        // Embed it in a UINavigationController
-        let navController = UINavigationController(rootViewController: userHoursVC)
-        
-        // Set the navigation controller as the root view controller
-        window.rootViewController = navController
-        self.window = window
-        window.makeKeyAndVisible()
-    } 
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
     
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
